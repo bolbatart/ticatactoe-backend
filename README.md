40b8f9198e649e384bc8ab62153c6bb58bc6240a
Artiom <bolbatart@gmail.com>
2020-07-01 14:57:32 +0300

    with dockerfile and docker-compose

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Tic Tac Toe game back end
+
+docker build -t reactappimage .
+docker run -it -p 3000:3000 reactappimage
--- test.js
+++
@@ -1,9 +0,0 @@
-const _ = require('lodash');
-
-const winning = [0, 4, 8];
-const crossesLines = [4, 0, 8];
-
-console.log(_.difference(winning, crossesLines).length === 0);
-
-// if (_.includes(crossesLines, winningLines[i])) return 'crosses';
-// if (_.includes(noughtsLines, winningLines[i])) return 'noughts';
